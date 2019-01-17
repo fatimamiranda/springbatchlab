69d8881cdb982c37009fa6aaab359f869ef321bb
sinensia500 <sinensia500@SINENSIA.EDU>
2019-01-17 23:47:53 +0100

    commit inicial

---
+++ src/main/java/com/pgrsoft/springbatchlab/SpringbatchlabApplication.java
@@ -0,0 +1,14 @@
+package com.pgrsoft.springbatchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchlabApplication.class, args);
+	}
+
+}
+
