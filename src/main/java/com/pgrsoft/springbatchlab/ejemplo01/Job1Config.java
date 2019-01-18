a5116505de7f98c6c9504ba7b063b57b85a5871f
sinensia500 <sinensia500@SINENSIA.EDU>
2019-01-18 13:48:53 +0100

    job1

---
+++ src/main/java/com/pgrsoft/springbatchlab/ejemplo01/Job1Config.java
@@ -0,0 +1,75 @@
+package com.pgrsoft.springbatchlab.ejemplo01;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class Job1Config {
+	
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Autowired
+	private DataSource dataSource;
+	
+	// Configuramos el Job
+	
+	@Bean
+	public Job job1() {
+		return jobBuilderFactory.get("Job1... ller Person de csv y guardar en tabla PEOPLE")
+				.flow(step1())
+				.end()
+				.build();
+	}
+	
+	// Configuramos el Step
+	
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+				.<Person,Person> chunk(10)
+				.reader(reader1())
+				.writer(writer1())
+				.build();
+	}
+	
+	
+	// Configuramos el reader
+
+	public FlatFileItemReader<Person> reader1(){
+		return new FlatFileItemReaderBuilder<Person>()
+			.name("reader1")
+			.resource(new FileSystemResource("materiales/entradas/ejemplo01_personas.csv"))
+			.delimited()
+			.names(new String[] {"firstName","lastName"})
+			.fieldSetMapper(new BeanWrapperFieldSetMapper<Person>(){{
+				setTargetType(Person.class);
+			}}).build();
+	}
+	
+	@Bean
+	public JdbcBatchItemWriter<Person> writer1(){
+		return new JdbcBatchItemWriterBuilder<Person>()
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.sql("INSERT INTO PEOPLE (first_name, last_name) VALUES (:firstName, :lastName)")
+				.dataSource(dataSource)
+				.build();
+	}
+}
--- src/main/java/com/pgrsoft/springbatchlab/triggers/TriggersController.java
+++ src/main/java/com/pgrsoft/springbatchlab/triggers/TriggersController.java
@@ -1,8 +1,47 @@
 package com.pgrsoft.springbatchlab.triggers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TriggersController {
 
+	@Autowired
+//	@Qualifier("syncJobLauncher")
+	@Qualifier("asyncJobLauncher")
+	private JobLauncher jobLauncher;
+	
+	@Autowired
+//	@Qualifier("job1")
+	private Job job1;
+	
+	@RequestMapping("/job1")
+	public String job1() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		
+		Map<String,JobParameter> jobParametersMap = new HashMap<>();
+		
+		jobParametersMap.put("parametro1", new JobParameter("p_" + System.currentTimeMillis()));
+		
+		JobParameters jobParameters = new JobParameters(jobParametersMap);
+		
+		jobLauncher.run(job1, jobParameters);
+		
+		return "ok";
+	}
+	
+	
+	
 }
