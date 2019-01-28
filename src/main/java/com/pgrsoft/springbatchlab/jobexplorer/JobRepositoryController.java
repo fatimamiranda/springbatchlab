40bc1c183acc51f29aefda8d916f4eb5b23e804c
jalemanyco <jordia4@gmail.com>
2019-01-28 21:40:11 +0100

    arreglos en jobexplorer.html

--- src/main/java/com/pgrsoft/springbatchlab/jobexplorer/JobRepositoryController.java
+++ src/main/java/com/pgrsoft/springbatchlab/jobexplorer/JobRepositoryController.java
@@ -2,63 +2,39 @@ package com.pgrsoft.springbatchlab.jobexplorer;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInstance;
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobRepositoryController {
-
-	@Autowired
-	private JobRepository jobRepository;
 	
 	@Autowired
 	private JobExplorer jobExplorer;
 	
-	@Autowired
-	private JobRegistry jobRegistry;
-	
 	@RequestMapping("/prueba")
-	public String prueba(Map<String, Object> model) {
-		
-		List<String> jobNames = jobExplorer.getJobNames();
+	public String prueba(Model model) {
 		
 		List<JobExecution> jobExecutions = new ArrayList<>();
 		
 		for (String jobName: jobExplorer.getJobNames()) {
-			List<JobInstance> jobInstances = jobExplorer.getJobInstances(jobName, 0, 1000);
+			
+			List<JobInstance> jobInstances = jobExplorer.getJobInstances(jobName, 0, 10000);
 			
 			for (JobInstance jobInstance: jobInstances) {
 				List<JobExecution> jobExecutionsForJobInstance = jobExplorer.getJobExecutions(jobInstance);
 				jobExecutions.addAll(jobExecutionsForJobInstance);
 			}
-			
 		}
 		
 		jobExecutions.sort((a,b) -> a.getId().compareTo(b.getId()));
-		
-		jobExecutions.stream().forEach(x-> {
-			System.out.println(x.getExitStatus());
-			System.out.println(x.getCreateTime());
-			System.out.println(x.getJobInstance().getJobName());
-			System.out.println(x.getJobParameters());
-			
-			
-			
-			System.out.println(x.getJobId());
-		});
-		
-		model.put("jobNames",jobNames);
-		model.put("jobExecutions",jobExecutions);
-		
-		System.out.println(jobExecutions);
+	
+		model.addAttribute("jobExecutions", jobExecutions);
 		
 		return "jobexplorer";
 	}
