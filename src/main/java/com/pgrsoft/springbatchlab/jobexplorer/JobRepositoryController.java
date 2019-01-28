4cfa9204d02acda585c74d7f766166397109323f
jalemanyco <jordia4@gmail.com>
2019-01-28 20:46:31 +0100

    html muestra tabla de jobExecution

--- src/main/java/com/pgrsoft/springbatchlab/jobexplorer/JobRepositoryController.java
+++ src/main/java/com/pgrsoft/springbatchlab/jobexplorer/JobRepositoryController.java
@@ -1,8 +1,11 @@
 package com.pgrsoft.springbatchlab.jobexplorer;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -25,15 +28,37 @@ public class JobRepositoryController {
 	@RequestMapping("/prueba")
 	public String prueba(Map<String, Object> model) {
 		
-		System.out.println(jobRepository);
-		
-		System.out.println(jobExplorer);
-		
-		System.out.println(jobRegistry);
-		
 		List<String> jobNames = jobExplorer.getJobNames();
 		
+		List<JobExecution> jobExecutions = new ArrayList<>();
+		
+		for (String jobName: jobExplorer.getJobNames()) {
+			List<JobInstance> jobInstances = jobExplorer.getJobInstances(jobName, 0, 1000);
+			
+			for (JobInstance jobInstance: jobInstances) {
+				List<JobExecution> jobExecutionsForJobInstance = jobExplorer.getJobExecutions(jobInstance);
+				jobExecutions.addAll(jobExecutionsForJobInstance);
+			}
+			
+		}
+		
+		jobExecutions.sort((a,b) -> a.getId().compareTo(b.getId()));
+		
+		jobExecutions.stream().forEach(x-> {
+			System.out.println(x.getExitStatus());
+			System.out.println(x.getCreateTime());
+			System.out.println(x.getJobInstance().getJobName());
+			System.out.println(x.getJobParameters());
+			
+			
+			
+			System.out.println(x.getJobId());
+		});
+		
 		model.put("jobNames",jobNames);
+		model.put("jobExecutions",jobExecutions);
+		
+		System.out.println(jobExecutions);
 		
 		return "jobexplorer";
 	}
